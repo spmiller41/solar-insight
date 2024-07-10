6c29542557506822b41437cd34c4eb2dd484b5fd
spmiller41 <spmiller41@gmail.com>
2024-07-09 23:19:06 -0400

    Commit git project initialization

---
+++ src/main/java/com/solar_insight/app/SolarInsightApplication.java
@@ -0,0 +1,25 @@
+package com.solar_insight.app;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SolarInsightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolarInsightApplication.class, args);
+	}
+
+	/*
+	@Bean
+	public CommandLineRunner demo() {
+		return (args) -> {
+
+		};
+	}
+	 */
+
+
+}
---
+++ src/test/java/com/solar_insight/app/SolarInsightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solar_insight.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolarInsightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
