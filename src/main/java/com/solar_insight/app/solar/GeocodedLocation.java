1412a42a0414b2c42b2588fc655ea90b724d7250
SeanyM41 <sean@gopowersolutions.com>
2024-07-14 18:59:01 -0400

    Commit GeocodedLocation classes to transfer lat/lng data

---
+++ src/main/java/com/solar_insight/app/solar/GeocodedLocation.java
@@ -0,0 +1,32 @@
+package com.solar_insight.app.solar;
+
+public class GeocodedLocation {
+
+    private double latitude;
+    private double longitude;
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    @Override
+    public String toString() {
+        return "GeocodedLocation{" +
+                "latitude=" + latitude +
+                ", longitude=" + longitude +
+                '}';
+    }
+
+}
