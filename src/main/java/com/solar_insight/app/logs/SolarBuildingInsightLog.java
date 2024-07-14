48248cbddee1f99b4717697eab8acb7831c4d936
SeanyM41 <sean@gopowersolutions.com>
2024-07-14 18:57:56 -0400

    Commit logging for SolarBuildingInsightService

---
+++ src/main/java/com/solar_insight/app/logs/SolarBuildingInsightLog.java
@@ -0,0 +1,14 @@
+package com.solar_insight.app.logs;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.solar_insight.app.solar.GeocodedLocation;
+import org.slf4j.Logger;
+
+public class SolarBuildingInsightLog {
+
+    public static void parsingError(GeocodedLocation location, JsonProcessingException ex, Logger logger) {
+        logger.error("Error parsing building insight response. Location: {}, {} Message: {}",
+                location.getLatitude(), location.getLongitude(), ex.getMessage());
+    }
+
+}
