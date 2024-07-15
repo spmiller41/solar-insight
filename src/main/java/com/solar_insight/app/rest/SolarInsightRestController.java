db5e5b9ffbc01e25eb81e602c8cc8d7a4fd13275
SeanyM41 <sean@gopowersolutions.com>
2024-07-14 21:51:20 -0400

    Commit update tor est controller - logging/testing purposes right now

--- src/main/java/com/solar_insight/app/rest/SolarInsightRestController.java
+++ src/main/java/com/solar_insight/app/rest/SolarInsightRestController.java
@@ -1,6 +1,10 @@
 package com.solar_insight.app.rest;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.solar_insight.app.solar.GeocodedLocation;
+import com.solar_insight.app.solar.SolarBuildingInsightService;
 import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +14,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class SolarInsightRestController {
 
+    private final SolarBuildingInsightService solarBuildingInsightService;
+
+    @Autowired
+    public SolarInsightRestController(SolarBuildingInsightService solarBuildingInsightService) {
+        this.solarBuildingInsightService = solarBuildingInsightService;
+    }
+
     @PostMapping("/preliminary_data")
     public void preliminaryDataController(@RequestBody PreliminaryDataDTO preliminaryDataDTO) {
         System.out.println(preliminaryDataDTO);
+
+        GeocodedLocation geocodedLocation =
+                new GeocodedLocation(preliminaryDataDTO.getLatitude(), preliminaryDataDTO.getLongitude());
+        System.out.println("Geocode: " + geocodedLocation);
+
+        JsonNode buildingInsightResponse = solarBuildingInsightService.getSolarData(geocodedLocation);
+        System.out.println(buildingInsightResponse);
     }
 }
