31afd612aef31a55e072fe7cf6c147670e9d47c5
SeanyM41 <sean@gopowersolutions.com>
2024-07-14 21:49:15 -0400

    Commit constructor to create instance of GeocodedLocation

--- src/main/java/com/solar_insight/app/solar/GeocodedLocation.java
+++ src/main/java/com/solar_insight/app/solar/GeocodedLocation.java
@@ -1,25 +1,6 @@
 package com.solar_insight.app.solar;
 
-public class GeocodedLocation {
-
-    private double latitude;
-    private double longitude;
-
-    public double getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(double latitude) {
-        this.latitude = latitude;
-    }
-
-    public double getLongitude() {
-        return longitude;
-    }
-
-    public void setLongitude(double longitude) {
-        this.longitude = longitude;
-    }
+public record GeocodedLocation(double latitude, double longitude) {
 
     @Override
     public String toString() {
