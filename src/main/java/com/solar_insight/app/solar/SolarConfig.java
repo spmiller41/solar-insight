67674f611f6be0472c4f345c62ab513fe6ddffc4
Sean <spmiller41@gmail.com>
2024-08-20 01:06:53 -0400

    Commit SolarConfig for static configutation data, and SolarConsumptionAnalyzer for discovering a customers needed consumption and price per kwh they currently pay

---
+++ src/main/java/com/solar_insight/app/solar/SolarConfig.java
@@ -0,0 +1,11 @@
+package com.solar_insight.app.solar;
+
+public class SolarConfig {
+    static final double DC_TO_AC_EFFICIENCY = 0.97;
+    static final double EFFICIENCY_DEPRECIATION_FACTOR = 0.995;
+    static final double COST_INCREASE_FACTOR = 1.022;
+    static final int PANEL_CAPACITY_WATTS = 400;
+    static final double INSTALLATION_COST_PER_WATT = 4.0;
+    static final int INSTALLATION_LIFE_SPAN = 20;
+    static final double INTEREST_RATE = 0.0599;
+}
---
+++ src/main/java/com/solar_insight/app/solar/SolarConsumptionAnalyzer.java
@@ -0,0 +1,49 @@
+package com.solar_insight.app.solar;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class SolarConsumptionAnalyzer {
+
+    private final double pricePerKwh;
+    private int annualAcKwhNeeded;
+
+    public SolarConsumptionAnalyzer(double costOfElectricityWithoutSolar, double initialAcKwhPerYear, double solarPercentage) {
+        this.pricePerKwh = calculateCurrentPricePerKWh(
+                costOfElectricityWithoutSolar, initialAcKwhPerYear, solarPercentage);
+    }
+
+    private double calculateCurrentPricePerKWh(double costOfElectricityWithoutSolar,
+                                               double initialAcKwhPerYear, double solarPercentage) {
+        // Constants - Based on Google Solar calculations.
+        final double COST_INCREASE_FACTOR = 0.022; // 2.2%
+        final int YEARS = 20; // 20 years
+
+        // Calculate the total annual consumption without solar
+        double totalEnergyConsumptionKWh = initialAcKwhPerYear / (solarPercentage / 100.0);
+
+        // Calculate the Present Value of the total cost by reversing inflation
+        double presentValueTotalCost = costOfElectricityWithoutSolar / Math.pow(1 + COST_INCREASE_FACTOR, YEARS);
+
+        // Total energy consumption over the specified number of years
+        double totalEnergyConsumptionOverYears = totalEnergyConsumptionKWh * YEARS;
+
+        // Current price per kWh
+        return new BigDecimal(presentValueTotalCost / totalEnergyConsumptionOverYears)
+                .setScale(3, RoundingMode.HALF_UP)
+                .doubleValue();
+    }
+
+    public void setAnnualAcKwhNeeded(int monthlyBill) {
+        this.annualAcKwhNeeded = (int) ((monthlyBill * 12) / pricePerKwh);
+    }
+
+    public int getAnnualAcKwhNeeded() {
+        return this.annualAcKwhNeeded;
+    }
+
+    public double getPricePerKwh() {
+        return this.pricePerKwh;
+    }
+
+}
