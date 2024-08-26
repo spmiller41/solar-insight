33db926a7d3bea5ed21a3dddc099b98d193a0fc2
spmiller41 <spmiller41@gmail.com>
2024-08-26 16:16:37 -0400

    Add SatelliteImageService to fetch HD satellite images using Google Maps API, returning image bytes with 2x scale and pin marker.

---
+++ src/main/java/com/solar_insight/app/solar/service/SatelliteImageService.java
@@ -0,0 +1,33 @@
+package com.solar_insight.app.solar.service;
+
+import com.solar_insight.app.GeocodedLocation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Service
+public class SatelliteImageService {
+
+    @Value("${google.maps.api.key}")
+    private String googleMapAPIKey;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public byte[] getSatelliteImage(GeocodedLocation geocodedLocation) {
+        String url = UriComponentsBuilder.fromHttpUrl("https://maps.googleapis.com/maps/api/staticmap")
+                .queryParam("center", geocodedLocation.latitude() + "," + geocodedLocation.longitude())
+                .queryParam("zoom", 19)
+                .queryParam("size", "400x200") // Maximum allowed size
+                .queryParam("scale", "2") // 2x scale for HD image (resulting in 1280x1280 image)
+                .queryParam("maptype", "satellite")
+                .queryParam("markers", geocodedLocation.latitude() + "," + geocodedLocation.longitude()) // Default Google Maps pin
+                .queryParam("key", googleMapAPIKey)
+                .toUriString();
+
+        return restTemplate.getForObject(url, byte[].class);
+    }
+
+}
