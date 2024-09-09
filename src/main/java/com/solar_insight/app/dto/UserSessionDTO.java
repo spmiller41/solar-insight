a813c2d72f2c7a9488f9df26ded3c3045700c64d
SeanyM41 <sean@gopowersolutions.com>
2024-09-09 01:48:58 -0400

    Commit additions to DTO

--- src/main/java/com/solar_insight/app/dto/UserSessionDTO.java
+++ src/main/java/com/solar_insight/app/dto/UserSessionDTO.java
@@ -1,4 +1,25 @@
 package com.solar_insight.app.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class UserSessionDTO {
+
+    @JsonProperty("sessionUUID")
+    private String sessionUUID;
+
+    public String getSessionUUID() {
+        return sessionUUID;
+    }
+
+    public void setSessionUUID(String sessionUUID) {
+        this.sessionUUID = sessionUUID;
+    }
+
+    @Override
+    public String toString() {
+        return "UserSessionDTO{" +
+                "sessionUUID='" + sessionUUID + '\'' +
+                '}';
+    }
+
 }
