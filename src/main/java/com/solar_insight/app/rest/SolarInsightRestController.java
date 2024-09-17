9642d9f0a533f6aeb6c122fe569cb75f49cc947e
spmiller41 <spmiller41@gmail.com>
2024-09-17 17:45:54 -0400

    Commit update to contactInfoController. This will return a generated booking query url back to the client/browser

--- src/main/java/com/solar_insight/app/rest/SolarInsightRestController.java
+++ src/main/java/com/solar_insight/app/rest/SolarInsightRestController.java
@@ -11,6 +11,7 @@ import com.solar_insight.app.google_solar.service.SatelliteImageService;
 import com.solar_insight.app.google_solar.service.SolarBuildingInsightService;
 import com.solar_insight.app.google_solar.utility.SolarConsumptionAnalyzer;
 import com.solar_insight.app.google_solar.utility.SolarOutcomeAnalysis;
+import com.solar_insight.app.ycbm.service.BookingUrlService;
 import com.solar_insight.app.zoho_crm.service.ZohoIntegrationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -30,24 +31,33 @@ public class SolarInsightRestController {
     private final SatelliteImageService imageService;
     private final SessionDataService sessionDataService;
     private final ZohoIntegrationService zohoIntegrationService;
+    private final BookingUrlService bookingUrlService;
 
     @Autowired
     public SolarInsightRestController(SolarBuildingInsightService solarBuildingInsightService,
                                       SatelliteImageService imageService,
                                       SessionDataService sessionDataService,
-                                      ZohoIntegrationService zohoIntegrationService) {
+                                      ZohoIntegrationService zohoIntegrationService,
+                                      BookingUrlService bookingUrlService) {
 
         this.solarBuildingInsightService = solarBuildingInsightService;
         this.imageService = imageService;
         this.sessionDataService = sessionDataService;
         this.zohoIntegrationService = zohoIntegrationService;
+        this.bookingUrlService = bookingUrlService;
     }
+
+
+
 
     // Health Check Endpoint
     @GetMapping("/status")
     public String status() {
         return "Solar Insight server is running...";
     }
+
+
+
 
     /**
      * This controller method handles the submission of preliminary data from the frontend.
@@ -76,26 +86,13 @@ public class SolarInsightRestController {
                 solarBuildingInsightService.idealSolarAnalysis(
                         buildingInsightResponse, consumptionAnalysis, preliminaryDataDTO.getAvgMonthlyEnergyBill());
 
-
         byte[] cachedImage = imageService.getSatelliteImage(geocodedLocation);
         String base64Image = Base64.getEncoder().encodeToString(cachedImage);
+        if (cachedImage != null) System.out.println("Satellite Image added to cache.");
 
-        if (cachedImage != null) {
-            // Proceed with sending the image and other data to Zoho
-            System.out.println("Image retrieved from cache.");
-        }
+        Map<String, String> response = createPayload(solarOutcome, base64Image, preliminaryDataDTO.getSessionUUID());
 
-        NumberFormat numberFormat = NumberFormat.getNumberInstance(Locale.US);
-        Map<String, String> response = new HashMap<>();
-        response.put("estimatedSavings", numberFormat.format(solarOutcome.getSavings()));
-        response.put("systemSizeDc", String.valueOf(solarOutcome.getYearlyDcSystemSize()));
-        response.put("monthlyPayment", String.valueOf(solarOutcome.getMonthlyBillWithSolar()));
-        response.put("incentives", numberFormat.format(solarOutcome.getSolarIncentives()));
-        response.put("panelCount", String.valueOf(solarOutcome.getPanelCount()));
-        response.put("imagery", base64Image);
-        response.put("uuid", preliminaryDataDTO.getSessionUUID());
-
-        try {
+        try { // Persist data
             sessionDataService.processUserSessionData(preliminaryDataDTO, solarOutcome);
         } catch (Exception ex) {
             // Add error logging here
@@ -105,17 +102,59 @@ public class SolarInsightRestController {
         return response;
     }
 
+
+
+
     @PostMapping("/address_confirm")
     public void addressConfirmController(@RequestBody UserSessionDTO userSessionDTO) {
         System.out.println("User Session UUID on Address Confirm: " + userSessionDTO.getSessionUUID());
         zohoIntegrationService.sendAddressAndEstimate(userSessionDTO);
     }
 
+
+
+
+    /**
+     * Handles the contact information submitted by the user.
+     * <p>
+     * If the lead is new (generated for the first time), it will be returned by processUserSessionData,
+     * and a record will be created in the database and in Zoho CRM with the contact details.
+     * <p>
+     * If no lead is returned (Optional.empty()), this should indicate the user is an existing lead,
+     * and only the current User Session will be updated in the database (no need to update Zoho CRM as the contact already exists).
+     * <p>
+     * Regardless of whether the lead is new or existing, a booking query URL is generated
+     * and sent back to the client, enabling the browser and YCBM to map potential appointments back to the server.
+     * <p>
+     * @param contactInfo the contact information provided by the user
+     * @return a response containing the booking page query URL
+     */
     @PostMapping("/contact_info")
-    public void contactInfoController(@RequestBody ContactInfoDTO contactInfo) {
-        System.out.println(contactInfo);
+    public Map<String, String> contactInfoController(@RequestBody ContactInfoDTO contactInfo) {
         Optional<ContactAddress> optGeneratedLead = sessionDataService.processUserSessionData(contactInfo);
-        optGeneratedLead.ifPresent(zohoIntegrationService::addContactToEstimate);
+        optGeneratedLead.ifPresent(zohoIntegrationService::addContactToEstimate); // Send to Zoho
+
+        Map<String, String> response = new HashMap<>();
+        response.put("bookingPageQueryUrl", bookingUrlService.buildQueryUrl(contactInfo.getSessionUUID()));
+        return response; // Return query URL
+    }
+
+
+
+
+    private Map<String, String> createPayload(SolarOutcomeAnalysis analysis, String base64Image, String sessionUUID) {
+        NumberFormat numberFormat = NumberFormat.getNumberInstance(Locale.US);
+
+        Map<String, String> response = new HashMap<>();
+        response.put("estimatedSavings", numberFormat.format(analysis.getSavings()));
+        response.put("systemSizeDc", String.valueOf(analysis.getYearlyDcSystemSize()));
+        response.put("monthlyPayment", String.valueOf(analysis.getMonthlyBillWithSolar()));
+        response.put("incentives", numberFormat.format(analysis.getSolarIncentives()));
+        response.put("panelCount", String.valueOf(analysis.getPanelCount()));
+        response.put("imagery", base64Image);
+        response.put("uuid", sessionUUID);
+
+        return response;
     }
 
 }
