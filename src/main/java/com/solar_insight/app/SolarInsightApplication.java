b99cc056b93b7654ecdef25d4b182aa7617eb7b8
spmiller41 <spmiller41@gmail.com>
2024-10-18 15:31:33 -0400

    Commit update to commandline runner for testing purposes

--- src/main/java/com/solar_insight/app/SolarInsightApplication.java
+++ src/main/java/com/solar_insight/app/SolarInsightApplication.java
@@ -1,19 +1,14 @@
 package com.solar_insight.app;
 
-import com.solar_insight.app.dao.ContactAddressDAO;
 import com.solar_insight.app.entity.Address;
-import com.solar_insight.app.entity.Contact;
-import com.solar_insight.app.entity.ContactAddress;
 import com.solar_insight.app.entity.SolarEstimate;
-import com.solar_insight.app.ycbm.service.BookingUrlService;
-import com.solar_insight.app.zoho_crm.enums.ZohoModuleAccess;
-import com.solar_insight.app.zoho_crm.service.TokenService;
-import com.solar_insight.app.zoho_crm.service.ZohoRequestService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class SolarInsightApplication {
@@ -22,27 +17,65 @@ public class SolarInsightApplication {
 		SpringApplication.run(SolarInsightApplication.class, args);
 	}
 
-	/*
-	@Autowired
-	private BookingUrlService bookingUrlService;
-
 	@Bean
 	public CommandLineRunner demo() {
 		return (args) -> {
-			ContactAddress contactAddress = new ContactAddress();
-			contactAddress.setAddressId(1);
-			contactAddress.setContactId(1);
-			contactAddress.setId(1);
+			String templateId = "tmpl_4b243da5ff5fd97";
+			String size = "6x9";
 
-			String sessionUUID= "01112ea8-1056-45cf-8012-6c312aff7d82";
+			Address address = new Address();
+			address.setStreet("254 Dogwood Road West");
+			address.setCity("Mastic Beach");
+			address.setState("NY");
+			address.setZip("11951");
 
-			String queryUrl = bookingUrlService.buildQueryUrl(contactAddress, sessionUUID);
-			System.out.
-			println("Booking Query URL: " + queryUrl);
+			SolarEstimate solarEstimate = new SolarEstimate();
+			solarEstimate.setEstimatedSavings(49500);
+			solarEstimate.setIncentives(13895);
+
+			Map<String, Object> mailerRequest = buildMailerRequest(address, solarEstimate, templateId, size);
+			System.out.println(mailerRequest);
+
 		};
 	}
-	 */
 
+	private Map<String, String> buildFromAddress() {
+		Map<String, String> fromAddress = new HashMap<>();
+		fromAddress.put("name", "Power Solutions");
+		fromAddress.put("address_line1", "2060 Ocean Ave");
+		fromAddress.put("address_city", "Ronkonkoma");
+		fromAddress.put("address_state", "NY");
+		fromAddress.put("address_zip", "11779");
+		fromAddress.put("address_country", "US");
+		return fromAddress;
+	}
+
+	private Map<String, Object> buildMailerRequest(Address address, SolarEstimate solarEstimate, String templateId, String size) {
+		Map<String, Object> request = new HashMap<>();
+
+		// Inline address object
+		Map<String, Object> toAddress = new HashMap<>();
+		toAddress.put("address_line1", address.getStreet());
+		toAddress.put("address_city", address.getCity());
+		toAddress.put("address_state", address.getState());
+		toAddress.put("address_zip", address.getZip());
+		toAddress.put("address_country", "US");
+
+		// Merge variables for template
+		Map<String, Object> mergeVariables = new HashMap<>();
+		mergeVariables.put("savings", solarEstimate.getEstimatedSavings());
+		mergeVariables.put("incentives", solarEstimate.getIncentives());
+
+		// Create request payload
+		request.put("to", toAddress);
+		request.put("from", buildFromAddress());
+		request.put("merge_variables", mergeVariables);
+		request.put("front", templateId);
+		request.put("size", size);
+		request.put("mail_type", "usps_first_class");
+
+		return request;
+	}
 
 }
 
